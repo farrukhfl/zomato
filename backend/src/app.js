31c7016f04b11f86b84fb99ceedfaba17c8f6dcd
Farrukh Shahzad <furrukhshahzad172@gmail.com>
2025-11-11 15:16:43 +0500

    Food Partner functionality created

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/auth.routes");
-// const foodRoutes = require("./routes/food.routes");
+const foodRoutes = require("./routes/food.routes");
 // const foodPartnerRoutes = require("./routes/food-partner.routes");
 const cors = require("cors");
 
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
-// app.use("/api/food", foodRoutes);
+app.use("/api/food", foodRoutes);
 // app.use("/api/food-partner", foodPartnerRoutes);
 
 module.exports = app;
